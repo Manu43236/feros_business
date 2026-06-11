47e3efc76e342f2e86c616eedd9f73ce5b18fa2c
manu <manikanta.chadaram1992@gmail.com>
2026-06-11 14:14:47 +0530

    base url updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api.feros.in/api/v1';
+const BASE_URL = 'https://console.feros.in/api/v1';
 
 async function post(path, body) {
   const res = await fetch(`${BASE_URL}${path}`, {
